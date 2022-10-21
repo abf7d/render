1c47345e5a1a44d5596419792544be6f5aa0a776
Aaron <afriedman111@gmail.com>
2022-10-21 14:30:15 -0400

    fix: added custom webpack

--- libs/viv-viewer/src/index.ts
+++ libs/viv-viewer/src/index.ts
@@ -1,4 +1,4 @@
 
 // export * from './lib/viv-viewer';
 // export * from './lib/jsx-viewer.jsx';
-export * from './lib/viv-viewer-web-comp-wrapper.jsx';
+export * from './lib/viv-viewer-web-comp-wrapper';
