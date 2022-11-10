dedce41447265efe3675c3fa3e83ed23273ba10e
N S <webgl@vt.edu>
2022-11-10 15:06:36 -0500

    Fix dependency conflicts

---
+++ libs/viv-viewer/src/lib/Avivator/src/layers/generateHeatmap.ts
@@ -0,0 +1,28 @@
+import {GridCellLayer} from 'deck.gl';
+console.log(GridCellLayer,'GridCellLayer')
+export const generateHeatmap = (
+  data: unknown,
+  cellSize: number,
+  visible: boolean,
+  zoomOpacity: number,
+  sliderOpacity: number,
+  selectedHeatmap: string
+) => {
+  console.log('generateHeatmap')
+  return new GridCellLayer({
+    id: 'grid-cell-layer-#detail#',
+    data,
+    //visible,
+    pickable: true,
+    autoHighlight: zoomOpacity > 0.5,
+    highlightColor: [255, 255, 255, 80],
+    extruded: false,
+    cellSize,
+    opacity: (selectedHeatmap === null || !visible) ? 0 : (zoomOpacity * sliderOpacity),
+    getPosition: (d: any) => d.position,
+    getFillColor: selectedHeatmap === null ? [0,0,0,0] : (d: any) => d.fillColors[selectedHeatmap],
+    updateTriggers: {
+      getFillColor: selectedHeatmap
+    }
+  });
+};
---
+++ libs/viv-viewer/src/lib/Avivator/src/layers/generatePolygons.ts
@@ -0,0 +1,18 @@
+import {PolygonLayer} from 'deck.gl';
+
+export const generatePolygons = (data: any, visible: boolean, opacity: number) => {
+  
+  return new PolygonLayer({
+    id: 'polygon-layer-#detail#',
+    data,
+    visible,
+    opacity,
+    pickable: true,
+    autoHighlight: true,
+    highlightColor: [255, 255, 255, 80],
+    lineWidthMinPixels: 3,
+    getPolygon: (d: any) => d.contour,
+    getFillColor: (d: any) => d.fillColor,
+    getLineColor: (d: any) => d.lineColor
+  });
+};
