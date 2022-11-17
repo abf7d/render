38fa32c37925cb2a1da1c98d6a92ced0812a9bc5
N S <webgl@vt.edu>
2022-11-16 19:15:15 -0500

    Rename index.jsx to viv-viewer-web-component.jsx

--- libs/viv-viewer/src/index.ts
+++ libs/viv-viewer/src/index.ts
@@ -2,4 +2,4 @@
 // export * from './lib/viv-viewer';
 // export * from './lib/jsx-viewer.jsx';
 // export * from './lib/viv-viewer-web-comp-wrapper.jsx';
-export * from './lib/Avivator/src/index.jsx';
+export * from './lib/Avivator/src/viv-viewer-web-component.jsx';
