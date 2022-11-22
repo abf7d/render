67976870fcaf2cd98b8bce17fa8ab6671c357d3f
N S <webgl@vt.edu>
2022-11-22 15:23:53 -0500

    Remove commented out code

--- libs/viv-viewer/src/index.ts
+++ libs/viv-viewer/src/index.ts
@@ -1,5 +1,1 @@
-
-// export * from './lib/viv-viewer';
-// export * from './lib/jsx-viewer.jsx';
-// export * from './lib/viv-viewer-web-comp-wrapper.jsx';
 export * from './lib/Avivator/src/viv-viewer-web-component.jsx';
