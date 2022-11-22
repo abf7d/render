16569f4f41e9edee82bcfb378deeaf69ac55ff1f
N S <webgl@vt.edu>
2022-11-22 16:17:16 -0500

    Remove commented out code

--- apps/render-ui/src/app/app.component.ts
+++ apps/render-ui/src/app/app.component.ts
@@ -21,12 +21,6 @@ export class AppComponent {
   ngOnInit(): void {
     const numberOfPlates = 50;
     const imageUrl$ = this.http.get(config.plateUrl)
-    /*
-      .subscribe((data: any) => {
-        this.imageUrls = data
-          .map((item: { name: string }) => config.plateUrl + item.name)
-          .slice(0, numberOfPlates);
-      });*/
       const overlayUrl$=this.http.get(config.overlayUrl);
       forkJoin([imageUrl$,overlayUrl$]).subscribe(([imageData, overlayData]: [any, any])=>{
         this.imageUrls = imageData
@@ -47,22 +41,5 @@ export class AppComponent {
             .map((d: any) => ({ label: d.id, value: d.id }))
             .concat({ label: 'None', value: null });
         });
-      /*.subscribe((data: any) => {
-        const processedData = processOverlayData({
-          overlayData: data.slice(0, numberOfPlates),
-          baseChemicalUrl: config.baseFormulaUrl,
-          cellSize: 3263,
-          plateSpacingX: 100000,
-          plateSpacingY: 100000,
-          platesPerRow: 20,
-          wellsPerRow: 24,
-          wellsPerColumn: 16,
-        });
-        this.overlayData=processedData;
-        this.heatmapIds = data[0].gridCellLayers
-          .map((d: any) => ({ label: d.id, value: d.id }))
-          .concat({ label: 'None', value: null });
-      });
-      */
   }
 }
