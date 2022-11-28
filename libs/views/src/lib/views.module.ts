c6a6a34bca4adf1b111af7c1d7cbb550dac77243
N S <webgl@vt.edu>
2022-11-28 13:00:43 -0500

    Generate publishable library and add component to it

--- libs/views/src/lib/views.module.ts
+++ libs/views/src/lib/views.module.ts
@@ -1,12 +1,7 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VivViewComponent } from './viv-view/viv-view.component';
-import { MenuComponentsModule } from '@render/menu-components';
 
 @NgModule({
-  imports: [CommonModule, MenuComponentsModule],
-  declarations: [VivViewComponent],
-  exports: [VivViewComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule],
 })
 export class ViewsModule {}
