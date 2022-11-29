27eef9f61d2dee37816da90db57f354832db0a6b
N S <webgl@vt.edu>
2022-11-29 17:46:08 -0500

    Specify export names

--- libs/viv-viewer/src/index.ts
+++ libs/viv-viewer/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/Avivator/src/viv-viewer-web-component.jsx';
-export * from './lib/Avivator/src/state.js';
-export * from './lib/Avivator/src/constants.js';
+export {useViewerStore, useChannelsStore} from './lib/Avivator/src/state.js';
+export {COLOR_PALLETE} from './lib/Avivator/src/constants.js';
