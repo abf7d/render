780a9e4e3c3c9675d3e5fdf3af1c5b74bb7ce562
N S <webgl@vt.edu>
2022-11-29 17:23:33 -0500

    Migrate to latest

--- libs/viv-viewer/jest.config.js
+++ libs/viv-viewer/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   displayName: 'viv-viewer',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/viv-viewer',
