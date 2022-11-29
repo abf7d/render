826ccd4bb0d8efb0cbd143391169e49d7e69b107
N S <webgl@vt.edu>
2022-11-29 18:23:47 -0500

    Make viv-viewer library buildable

--- libs/viv-viewer/jest.config.ts
+++ libs/viv-viewer/jest.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'viv-viewer',
   preset: '../../jest.preset.js',
   transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
