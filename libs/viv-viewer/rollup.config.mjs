fafc7c96f9cc8ff49dcf2d3ee72c12c5d5a825bb
Aaron <afriedman111@gmail.com>
2022-12-12 14:03:16 -0500

    fix: working esm and cjs files generating

--- libs/viv-viewer/rollup.config.mjs
+++ libs/viv-viewer/rollup.config.mjs
@@ -6,6 +6,14 @@ import { babel } from '@rollup/plugin-babel';
 
 import packageJson from "./package.json" assert { type: "json" };
 
+
+// https://dev.to/siddharthvenkatesh/component-library-setup-with-react-typescript-and-rollup-onj
+// rollup-plugin-postcss - To handle our css
+// rollup-plugin-terser - To minify our bundle
+
+// jsx transform
+// https://stackoverflow.com/questions/67811412/rollup-react-17-with-new-jsx-transform-react-is-not-defined
+
 export default [
   {
     input: "src/index.ts",
@@ -15,20 +23,62 @@ export default [
       //   format: "cjs",
       //   sourcemap: true,
       // },
+      
+      // Creates esm folder with index.js with what looks like the code base
       {
-        dir: 'dist',
-        // file: packageJson.module,
+        file: packageJson.module,
         format: "esm",
         sourcemap: true,
+        inlineDynamicImports: true
       },
+
+      /// cjs, cretes many files but index.js refers to a file with what appears to be the code base
+      // {
+      //   dir: 'dist',
+      //   // file: packageJson.module,
+      //   format: "cjs",
+      //   sourcemap: true,
+      // },
+
+
+      /// using cjs and file instead of directory with inlineDyanmaic imports 
+      {
+        //dir: 'dist',
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+
+
+
+      
+      // {
+      //   dir: 'dist',
+      //   // file: packageJson.module,
+      //   format: "esm",
+      //   sourcemap: true,
+      // },
+
+
+      
     ],
     plugins: [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
+      babel({ babelHelpers: 'bundled' }),
       
     ],
+
+    /// excluding node_modules folder
+    // plugins: [
+    //   resolve(),
+    //   commonjs(),
+    //   typescript({ tsconfig: "./tsconfig.json" }),
+    //   babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
+      
+    // ],
   },
   {
     input: "dist/esm/index.d.ts",
