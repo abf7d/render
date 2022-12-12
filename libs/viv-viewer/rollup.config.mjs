7aede2d5ad2b509c557b22465e23a4190fc2d1a2
Aaron <afriedman111@gmail.com>
2022-12-12 09:22:54 -0500

    fix: initial rollup

---
+++ libs/viv-viewer/rollup.config.mjs
@@ -0,0 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { babel } from '@rollup/plugin-babel';
+
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      // {
+      //   file: packageJson.main,
+      //   format: "cjs",
+      //   sourcemap: true,
+      // },
+      {
+        dir: 'dist',
+        // file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
+      
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
